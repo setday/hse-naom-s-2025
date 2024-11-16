dbbaa74c54b138ccaaea8865026019348863b90c
Artem Batygin <aa.batygin@gmail.com>
2024-11-16 23:03:14 +0300

    Update OpenBLAS LSE solver

--- lab01/include/lse_solvers/openblas_solver.h
+++ lab01/include/lse_solvers/openblas_solver.h
@@ -2,8 +2,14 @@
 
 #include <cstddef>
 #include <cstdlib>
-#include <gsl/gsl_blas.h>
-#include <lapacke.h>
+#include <cstring>
+#include <openblas_config.h>
+
+extern "C"
+{
+  void dgetrf_( const int* m, const int* n, double* a, const int* lda, int* ipiv, int* info );
+  void dgetrs_( const char* trans, const int* n, const int* nrhs, const double* a, const int* lda, const int* ipiv, double* b, const int* ldb, int* info );
+}
 
 namespace ADAAI::LSE_SOLVERS
 {
@@ -22,15 +28,45 @@ namespace ADAAI::LSE_SOLVERS
  */
 void solve_linear_system_openblas( std::size_t N, double** A, double* x, const double* b )
 {
-  int* ipiv = ( int* ) std::malloc( N * sizeof( *ipiv ) );
+  int* ipiv = reinterpret_cast<int*>( std::malloc( N * sizeof( *ipiv ) ) );
+  if ( ipiv == nullptr )
+  {
+    throw std::bad_alloc();
+  }
+  double* C = reinterpret_cast<double*>( std::malloc( N * N * sizeof( *C ) ) );
+  if ( C == nullptr )
+  {
+    free( ipiv );
+    throw std::bad_alloc();
+  }
+
   std::memcpy( x, b, N * sizeof( double ) );
-  double* C = ( double* ) std::malloc( N * N * sizeof( *C ) );
   for ( std::size_t i = 0; i < N; ++i )
   {
     std::memcpy( C + i * N, A[i], N * sizeof( double ) );
   }
-  LAPACKE_dgetrf( LAPACK_COL_MAJOR, N, N, C, N, ipiv );
-  LAPACKE_dgetrs( LAPACK_COL_MAJOR, 'N', N, 1, C, N, ipiv, x, N );
+
+  int info;
+
+  int n_int = static_cast<int>( N );
+  dgetrf_( &n_int, &n_int, C, &n_int, ipiv, &info );
+  if ( info != 0 )
+  {
+    free( C );
+    free( ipiv );
+    throw std::runtime_error( "LU decomposition failed" );
+  }
+
+  char trans = 'N';
+  int  nrhs  = 1;
+  dgetrs_( &trans, &n_int, &nrhs, C, &n_int, ipiv, x, &n_int, &info );
+  if ( info != 0 )
+  {
+    free( C );
+    free( ipiv );
+    throw std::runtime_error( "Solving the linear system failed" );
+  }
+
   free( C );
   free( ipiv );
 }
